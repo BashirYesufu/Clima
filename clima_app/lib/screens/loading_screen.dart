63d5ee507617576415d93d5f233f89754bf5a512
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-13 00:50:05 +0100

    Loaded location from when the app loads

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -9,12 +9,18 @@ class LoadingScreen extends StatefulWidget {
 }
 
 void getLocation() async {
-  await Geolocator.requestPermission();
-  Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  print(position);
+  Location location = Location();
+  await location.getCurrentLocation();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
