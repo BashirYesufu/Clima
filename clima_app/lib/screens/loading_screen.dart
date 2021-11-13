73592be967e16dabcba40528f9c046a745ece3b4
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-13 03:08:15 +0100

    Decoded data from the API

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -13,12 +15,31 @@ void getLocation() async {
   await location.getCurrentLocation();
 }
 
+void getData() async {
+  http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+
+  if (response.statusCode == 200) {
+    String data = response.body;
+    var decodeData = jsonDecode(data);
+    double temperature = decodeData['main']['temp'];
+    int condition = decodeData['weather'][0]['id'];
+    String cityName = decodeData['name'];
+
+    print(temperature);
+    print(condition);
+    print(cityName);
+  } else {
+    print(response.statusCode);
+  }
+}
+
 class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   @override
@@ -28,7 +49,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
-
             getLocation();
           },
           child: Text('Get Location'),
